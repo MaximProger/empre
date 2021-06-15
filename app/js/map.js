1c529c2da81cb081356971cbaddef0b3614e2471
Maksim Koshman <maksimkoshman090@gmail.com>
2021-06-15 08:31:15 +0300

    Завершил десктопную версию главной страницы

---
+++ app/js/map.js
@@ -0,0 +1,38 @@
+ymaps.ready(function () {
+  var myMap = new ymaps.Map(
+      "map",
+      {
+        center: [53.432701, 59.080696],
+        zoom: 13,
+        controls: [],
+      },
+      {
+        searchControlProvider: "yandex#search",
+      }
+    ),
+    // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
+    myPlacemark = new ymaps.Placemark(
+      myMap.getCenter(),
+      {
+        hintContent: "Собственный значок метки",
+        balloonContent: "Это красивая метка",
+      },
+      {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: "default#image",
+        // Своё изображение иконки метки.
+        iconImageHref: "images/dest/picker.svg",
+        // Размеры метки.
+        iconImageSize: [49, 78],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-5, -38],
+      }
+    );
+
+  myMap.geoObjects.add(myPlacemark);
+});
